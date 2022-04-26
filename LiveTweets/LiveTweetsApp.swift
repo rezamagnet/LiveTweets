b44ed0e2ff0881530e0f903f17a3b1692bcdfc13
rezamagnet <rezakhonsari69@gmail.com>
2022-04-27 02:05:53 +0430

    Initial Commit

---
+++ LiveTweets/LiveTweetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTweetsApp.swift
+//  LiveTweets
+//
+//  Created by Reza Khonsari on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveTweetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
